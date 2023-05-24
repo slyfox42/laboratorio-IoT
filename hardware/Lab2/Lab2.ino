f68f343c18cd24a0b76f29bb8d8ce4effe9eb46f
Federico Obialero <federico.obialero@gmail.com>
2023-05-24 15:08:52 +0200

    moved temp code to a function, temporarily removed fan movement

--- hardware/Lab2/Lab2.ino
+++ hardware/Lab2/Lab2.ino
@@ -3,7 +3,7 @@
 #define FAN_PIN A1
 #define pinTempSensor A0 
 #define minTemp 25
-#define maxTemp 30
+#define maxTemp 35 // TODO: 
 
 const int B = 4275;               // B value of the thermistor
 const int R0 = 100000;            // R0 = 100k
@@ -17,31 +17,38 @@ void setup() {
 
 }
 
-void loop() {
-
-  float speed = 0;
-
+// code for grove temp sensor to acquire and convert temperature to Celsius
+int readTemp(int pin) {
   int a = analogRead(pinTempSensor);
 
   float R = 1023.0/a-1.0;
   R = R0*R;
 
   float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
-
+  
   Serial.print("temperature = ");
   Serial.println(temperature);
+
+  return temperature;
+}
+
+void loop() {
+
+  float speed = 0;
+
+  int temperature = readTemp(pinTempSensor);
 
   delay(100);
   
   if(temperature >= minTemp){
     if(temperature > maxTemp){
-      analogWrite(FAN_PIN, 255);
+      speed = 255;
     }
     else{
       speed = 255 - (maxTemp - temperature)*20;
-      analogWrite(FAN_PIN, speed);
     }
   }
+  // analogWrite(FAN_PIN, speed);
   Serial.println(speed);
 
   delay(2000);
