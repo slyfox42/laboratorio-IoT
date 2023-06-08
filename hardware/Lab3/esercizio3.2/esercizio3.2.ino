469ce460477a7fa8fb6e2ab0d47058a3d6ae9ede
Federico Obialero <federico.obialero@gmail.com>
2023-06-08 11:28:46 +0200

    moved wifi utilities outside the main .ino folder

--- hardware/Lab3/esercizio3.2/esercizio3.2.ino
+++ hardware/Lab3/esercizio3.2/esercizio3.2.ino
@@ -1,17 +1,18 @@
 #include <WiFiNINA.h>
 #include <ArduinoHttpClient.h>
 #include <ArduinoJson.h>
+#include "utilities.h"
 #include "arduino_secrets.h"
 #define pinTempSensor A0
 
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, serverAddress, serverPort);
+
 const int B = 4275;               // B value of the thermistor
 const int R0 = 100000;    
-int status = WL_IDLE_STATUS;
 const int capacity = JSON_OBJECT_SIZE(2) + JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(4) + 100;
 DynamicJsonDocument jsonResponse(capacity);
 
-WiFiClient wifi;
-HttpClient client = HttpClient(wifi, server_address, server_port);
 
 void setup() {
 
@@ -19,55 +20,12 @@ void setup() {
   while (!Serial);
   
   enable_WiFi();
-  connect_WiFi();
+  connect_WiFi(wifiSsid, wifiPass);
 
   printWifiStatus();
-
 }
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
-
-}
-
-void enable_WiFi() {
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
-
-  String fv = WiFi.firmwareVersion();
-  if (fv < "1.0.0") {
-    Serial.println("Please upgrade the firmware");
-  }
-}
-
-void connect_WiFi() {
-  // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass); // connect to wifi using name and pass
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-}
 
 float readTemp(int pin) {
   int a = analogRead(pinTempSensor);
