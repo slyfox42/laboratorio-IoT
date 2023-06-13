88c713ef24010243898f9c1d1ba8189ae5ae2d0f
Federico Obialero <federico.obialero@gmail.com>
2023-06-13 16:56:25 +0200

    wip, need to add subscription for software labs

--- hardware/Lab3/esercizio3.3/esercizio3.3.ino
+++ hardware/Lab3/esercizio3.3/esercizio3.3.ino
@@ -5,51 +5,64 @@
 #include "utilities.h"
 
 #define pinTempSensor A0
+#define ledPin A2
 
 WiFiClient wifi;
 const int B = 4275;
 String broker_address = "test.mosquitto.org";
 int broker_port = 1883;
-int status = WL_IDLE_STATUS;
-const String base_topic = "/tiot/8";
+const String base_topic = "/tiot/group8";
 const int capacity = JSON_OBJECT_SIZE(2) + JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(4) + 100;
-DynamicJsonDocument doc_snd(capacity);
-DynamicJsonDocument doc_rec(capacity);
+DynamicJsonDocument jsonReceived(capacity);
+DynamicJsonDocument jsonResponse(capacity);
 
 
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  DeserializationError err = deserializeJson(doc_rec, (char*) payload);
-  if (err) {
-    Serial.print(F("deserializeJson() failed with code "));
-    Serial.println(err.c_str());
+  if (topic == (base_topic + "/led")) {
+    DeserializationError err = deserializeJson(jsonReceived, (char*) payload);
+    if (err) {
+      Serial.print(F("deserializeJson() failed with code "));
+      Serial.println(err.c_str());
+      return;
+    }
+    if (jsonReceived["e"][0]["n"] == "led") {
+      int value = jsonReceived["e"][0]["v"];
+      if (value == 1 || value == 0) {
+        digitalWrite(ledPin, value);
+        Serial.println("Led value set.");
+        return;
+      } else {
+        Serial.println("Invalid LED value received.");
+        return;
+      }
+    }
   }
-
+  
 }
 
 PubSubClient client(broker_address.c_str(), broker_port, callback, wifi);
 
+String senMlEncode(float temperature) {
+  String body;
+  jsonResponse.clear();
+  jsonResponse["bn"] =  "ArduinoGroupX";
+  jsonResponse["e"][0]["t"] = int(millis()/1000);
+  jsonResponse["e"][0]["n"] = "temperature"; 
+  jsonResponse["e"][0]["v"] = temperature; 
+  jsonResponse["e"][0]["u"] = "Cel"; 
+  serializeJson(jsonResponse, body);
 
-// String senMlEncode(float temperature) {
-//   String body;
-//   jsonResponse.clear();
-//   jsonResponse["bn"] =  "ArduinoGroupX";
-//   jsonResponse["e"][0]["t"] = int(millis()/1000);
-//   jsonResponse["e"][0]["n"] = "temperature"; 
-//   jsonResponse["e"][0]["v"] = temperature; 
-//   jsonResponse["e"][0]["u"] = "Cel"; 
-//   serializeJson(jsonResponse, body);
-
-//   return body;
-// }
+  return body;
+}
 
 void setup() {
-
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
   while (!Serial);
 
   enable_WiFi();
-  connect_WiFi();
+  connect_WiFi(wifiSsid, wifiPass);
 
   printWifiStatus();
 }
@@ -67,8 +80,33 @@ float readTemp(int pin) {
   return temperature;
 }
 
+void reconnect() {
+  while (client.state() != MQTT_CONNECTED) {
+    if (client.connect("TiotGroup8")) {
+      client.subscribe((base_topic + String("/led")).c_str());
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds...");
+      delay(5000);
+    }
+  }
+}
 
 void loop() {
+  if (client.state() != MQTT_CONNECTED) {
+    reconnect();
+  }
+  Serial.println("Reading temperature...");
   float temperature = readTemp(pinTempSensor);
+  String body = senMlEncode(temperature);
+  
+  Serial.println("Publishing data to broker...");
+  client.publish((base_topic + String("/temperature")).c_str(), body.c_str());
+  
+  Serial.println("Checking for new messages on subscribed topics...");
+  client.loop();
 
+  delay(10000);
 }
+
