43c683a1ea0a78ad7a269dcb64340b17f9da7d75
Federico Obialero <federico.obialero@gmail.com>
2023-06-14 14:47:44 +0200

    removed arduino secrets, adding to gitignore

--- hardware/Lab3/esercizio3.2/arduino_secrets.h
+++
@@ -1,4 +0,0 @@
-extern char wifiSsid[] = "";
-extern char wifiPass[] = "";
-extern char serverAddress[] = "";
-extern int serverPort = 80;
--- hardware/Lab3/esercizio3.3/arduino_secrets.h
+++
@@ -1,4 +0,0 @@
-extern char wifiSsid[] = "";
-extern char wifiPass[] = "";
-extern char server_address[] = "";
-extern int server_port = 80;
