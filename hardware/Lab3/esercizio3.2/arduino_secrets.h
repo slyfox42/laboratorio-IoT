0293cc5022142f9dd4d948b7da73c9d2f519ab30
Federico Obialero <federico.obialero@gmail.com>
2023-06-25 22:42:12 +0200

    added arduino_secrets file

---
+++ hardware/Lab3/esercizio3.2/arduino_secrets.h
@@ -0,0 +1,5 @@
+extern char wifiSsid[] = "";
+extern char wifiPass[] = "";
+extern char serverAddress[] = "";
+extern int catalogServerPort = 8080;
+extern int temperatureServerPort = 8081;
---
+++ hardware/Lab3/esercizio3.3/arduino_secrets.h
@@ -0,0 +1,4 @@
+extern char wifiSsid[] = "";
+extern char wifiPass[] = "";
+extern char serverAddress[] = "";
+extern int serverPort = 8080;
